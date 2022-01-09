432a6bce97e136397e9a914dbebc549b03aa5de4
Ujjen Raj <ujjenrajkarnikar@gmail.com>
2022-01-09 16:07:08 -0600

    GUI

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,10 +1,25 @@
 from tkinter import *
 from random import *
 
+
+
+######MAIN PROGRAM LOOP#######
 WIDTH = 800
 HEIGHT = 800
 window = Tk()
 window.geometry("{}x{}".format(WIDTH, HEIGHT))
 window.title("GUI demo")
 window.config(bg='BLACK')
+
+# Label
+l = Label(window, text = "Voice Authentication System", bg = "black", bd = 100, fg = "white", justify = RIGHT, padx = 10, pady = 10, font=("Helvetica", 26))
+l.pack()
+
+# buttons
+b1 = Button(window, text="button 1", bd = 5, bg = "pink", fg = "blue", font=("Helvetica", 12), justify = CENTER)
+b1.pack()
+
+b2 = Button(window, text="button 2", bd = 5, bg = "pink", fg = "blue", font=("Helvetica", 12), justify = CENTER)
+b2.pack()
+
 window.mainloop()
