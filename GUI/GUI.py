603630987d2fbdbfbb008bfd95025313dfb48d97
Ujjen Raj <ujjenrajkarnikar@gmail.com>
2022-01-10 21:05:44 -0600

    added 3rd button

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from random import *
-
+from tkinter.filedialog import askopenfilename,asksaveasfilename
 
 
 ######MAIN PROGRAM LOOP#######
@@ -21,6 +21,9 @@ b1.place(x=100, y=400)
 
 b2 = Button(window, text="button 2", bg = "pink", fg = "blue", font=("Helvetica", 12), justify = CENTER)
 b2.place(x=600, y=400)
+
+b3 = Button(window, text="Open", bg = "pink", fg = "blue", font=("Helvetica", 12), justify = CENTER)
+b3.place(x=400, y=400)
 
 
 window.mainloop()
