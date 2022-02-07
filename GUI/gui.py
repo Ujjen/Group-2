8e083df12d232d9b61ff1627d8a92b74eeda288b
Ujjen Raj <ujjenrajkarnikar@gmail.com>
2022-02-06 20:27:23 -0600

    plot

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-import frequencies
+from frequencies import plot
 window = tk.Tk()
 
-frame1 = tk.Frame(master=window, width=100, height=100, bg = "frequencies")
+frame1 = tk.Frame(master=window, width=100, height=100, bg = "plot")
 frame1.pack()
 
 window.mainloop()
