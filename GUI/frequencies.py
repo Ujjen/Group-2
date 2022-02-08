8d0a81b33f97ea8cc2b333971e8708f08feb2aab
Ryan Erbelding <ess015@email.latech.edu>
2022-02-08 14:23:54 -0600

    fix merging

---
+++ GUI/frequencies.py
@@ -0,0 +1,126 @@
+from tkinter import *
+from tkinter.filedialog import askopenfilename,asksaveasfilename
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from scipy.io.wavfile import read
+from time import sleep
+import speech_recognition as sr
+import wave
+#import RPi.GPIO as GPIO
+import math
+import subprocess
+import keyboard
+
+
+def distance():
+    input_data = read(r"C:\Users\r2d26\OneDrive\Desktop\Elenahelpstuff\speech.wav")
+    password_data = read(r"C:\Users\r2d26\OneDrive\Desktop\Elenahelpstuff\speech2.wav")
+    audio2 = password_data[1]
+    audio = input_data[1]
+
+    x=(sum(audio2) / len(audio2))
+    y = (sum(audio) / len(audio2))
+
+    print(x)
+    print(y)
+
+    print((x+y)/2)
+    return True
+
+        
+def plot():
+    fig = Figure(figsize = (5,5), dpi = 100)
+    
+    password_data = read(r"C:\Users\r2d26\OneDrive\Desktop\Elenahelpstuff\speech.wav")#
+    input_data = read(r"C:\Users\r2d26\OneDrive\Desktop\Elenahelpstuff\speech2.wav")
+    input1 = input_data[1]
+    
+    password3 = password_data[1]
+
+    plot1 = fig.add_subplot(111)
+    plot1.plot(input1)
+
+    plot2 = fig.add_subplot(212)
+    plot2.plot(password3)
+    
+    canvas = FigureCanvasTkAgg(fig, master = window)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    
+    distance()
+
+
+button = 25
+running = True
+
+
+
+
+
+
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+a = sr.Recognizer()
+
+
+with sr.Microphone() as source:
+    print("Record a password...")
+    audio = a.listen(source)
+
+        # new addition to save the recording
+    with open(r"C:\Users\r2d26\OneDrive\Desktop\Elenahelpstuff\speech.wav", 'wb') as f:
+        f.write(audio.get_wav_data())
+
+        temp = read(r"C:\Users\r2d26\OneDrive\Desktop\Elenahelpstuff\speech.wav")
+        temp_list = temp[1]
+        tempAvg = sum(temp) / len(temp)
+        data=a.recognize_google(audio)
+        print("password saved.")
+        
+while running:
+    if (keyboard.is_pressed('ctrl')):
+        with sr.Microphone() as source:
+            b = sr.Recognizer()
+            print("Say something...")
+            audio2 = b.listen(source)
+            
+            # new addition to save the recording
+            with open(r"C:\Users\r2d26\OneDrive\Desktop\Elenahelpstuff\speech2.wav", 'wb') as d:
+                d.write(audio2.get_wav_data())
+                
+            data2=b.recognize_google(audio2)
+
+            distance()
+            if(data2 == data):
+                print("correct password!!")
+                subprocess.call(["xdg-open", '/home/pi/Desktop/images-gif'])
+                print("password: {}".format(data))
+                print("password attempt: {}".format(data2))
+                plot()
+
+                if (distance() == True):
+                    # unlock the file or whatever
+                    print("LETS FUCKING GOOOOOOOOOOOOO")
+                    break
+                else:
+                    continue
+            else:
+                print("wrong password try again")
+                
+    elif(keyboard.is_pressed('ctrl')):
+        print("Push the button to record audio...")
+        sleep(5)
+        if(keyboard.is_pressed('ctrl')):
+            running = True
+        else:
+            running = False
+
+window = Tk()
+window.geometry("500x500")
+
+window.mainloop()
+
+        
+
