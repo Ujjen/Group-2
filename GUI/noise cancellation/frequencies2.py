3e817160c090bbdbc124528e164c41c93bf1fa46
Ujjen Raj <ujjenrajkarnikar@gmail.com>
2022-02-08 22:50:38 -0600

    final5

--- GUI/noise cancellation/frequencies2.py
+++ GUI/noise cancellation/frequencies2.py
@@ -26,7 +26,7 @@ def distance():
     # if statement to decide if the input is close enough to the avg password data
     print("avg "+str(avg))
     print("avg input "+str(avg_input))
-    if (avg_input >= avg - 5) and (avg_input <= avg + 5):
+    if (avg_input >= avg - 1) and (avg_input <= avg + 1):
         print(avg_input)
         print(avg)
         return True
