6331e7aae05758fbbb02f2d1bdc5d90e52d917ef
Elena <ess015@latech.edu>
2022-02-08 21:00:27 -0600

    I THINK I DID ITITITITI

--- GUI/frequencies.py
+++ GUI/frequencies.py
@@ -11,107 +11,87 @@ import RPi.GPIO as GPIO
 import math
 import subprocess
 
-
-def distance():
-    input_data = read(r"/home/pi/speech.wav")
-    password_data = read(r"/home/pi/speech2.wav")
-    audio2 = password_data[1]
-    audio = input_data[1]
-
-    x=(sum(audio2) / len(audio2))
-    y = (sum(audio) / len(audio2))
-
-    print(x)
-    print(y)
-
-    print((x+y)/2)
-    return True
-
-        
-def plot():
-
-
-    fig = Figure(figsize = (5,5), dpi = 100)
-    
-
-    plt.figure(1, figsize = (30, 10))
-
-    password_data = read(r"/home/pi/speech.wav")
-    input_data = read(r"/home/pi/speech2.wav")
-
-    fig = Figure(figsize = (7,7), dpi = 100)
-    
-
-    password_data = read(r"/home/pi/speech.wav")#
-    input_data = read(r"/home/pi/speech2.wav")
-
-    input1 = input_data[1]
-    
-    password3 = password_data[1]
-
-    plot1 = fig.add_subplot(111)
-    #plot1 = fig.add_subplot(150)
-    plot1.plot(input1)
-
-    plot2 = fig.add_subplot(212)
-    #plot2 = fig.add_subplot(275)
-    plot2.plot(password3)
-    
-    canvas = FigureCanvasTkAgg(fig, master = "window")
-    canvas.draw()
-    canvas.get_tk_widget().pack()
-    
-    distance()
-
 def clear():
     exit()
 
+def distance():
+    input_data = read(r"/home/pi/Desktop/wavfiles/speech2.wav")
+    password_data = read(r"/home/pi/Desktop/wavfiles/speech.wav")
+    audio2 = password_data[1]
+    audio = input_data[1]
+
+    avg_input = (sum(audio) / len(audio))
+    
+    # if statement to decide if the input is close enough to the avg password data
+    print("avg "+str(avg))
+    print("avg input "+str(avg_input))
+    if (avg_input >= avg - 2) and (avg_input <= avg + 2):
+        print(avg_input)
+        print(avg)
+        return True
+    else:
+        return False
+    
+
+        
+def plot():
+    fig = Figure(figsize = (10,10), dpi = 65)
+    
+    password_data = read(r"/home/pi/Desktop/wavfiles/speech.wav")#
+    input_data = read(r"/home/pi/Desktop/wavfiles/speech2.wav")
+    input1 = input_data[1]
+    
+    password = password_data[1]
+
+    plot1 = fig.add_subplot(211)
+    plot1.set_title("Password Data")
+    plot1.plot(password)
+    
+
+    plot2 = fig.add_subplot(212)
+    plot2.plot(input1)
+    plot2.set_title("Input Data")
+    
+    canvas = FigureCanvasTkAgg(fig, master = window)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    
+
+
 button = 25
 running = True
-
-
+denied = False
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+window = Tk()
+window.geometry("800x800")
+clear_button = Button(master = window, text = "Clear", command = lambda : clear())
+clear_button.pack()
+
 a = sr.Recognizer()
-avgL = [] 
-for i in range(0,3):
+
+avgL=[]
+
+for i in range (0,3):
     with sr.Microphone() as source:
         print("Record a password...")
         audio = a.listen(source)
 
         # new addition to save the recording
-        with open(r"/home/pi/speech.wav", 'wb') as f:
+        with open(r"/home/pi/Desktop/wavfiles/speech.wav", 'wb') as f:
             f.write(audio.get_wav_data())
 
-
-
-        ###
-            temp_data = read(r"/home/pi/speech.wav")
-            temp = temp_data[1]
-            avgL.append(sum(temp)/len(temp))
-        ###
-
+            temp = read(r"/home/pi/Desktop/wavfiles/speech.wav")
+            avgL.append(sum(temp[1]) / len(temp[1]))
             data=a.recognize_google(audio)
             print("password saved.")
 
-###
-    avg = (sum(avgL) / len(avgL))
-###
+print(avgL)
+avg = (sum(avgL) / len(avgL))
+print(avg)
 
-
-
-### GUI #####################
-
-window = Tk()
-window.geometry("500x500")
-clear_button = Button(master = window, command = clear, height = 2, width = 10, text = "Clear")
-clear_button.pack()
-
-############################
-
-    
 while running:
     if (GPIO.input(button) == GPIO.HIGH):
         with sr.Microphone() as source:
@@ -120,35 +100,41 @@ while running:
             audio2 = b.listen(source)
             
             # new addition to save the recording
-            with open(r"/home/pi/speech2.wav", 'wb') as d:
+            with open(r"/home/pi/Desktop/wavfiles/speech2.wav", 'wb') as d:
                 d.write(audio2.get_wav_data())
                 
             data2=b.recognize_google(audio2)
-            print(data2)
+            print("password: {}".format(data))
 
-            distance()
             if(data2 == data):
-                print("correct password!!")
-                subprocess.call(["xdg-open", '/home/pi/Desktop/images-gif'])
-                print("password: {}".format(data))
+                #print("password: {}".format(data))
                 print("password attempt: {}".format(data2))
                 plot()
 
                 if (distance() == True):
                     # unlock the file or whatever
-                    print("LETS FUCKING GOOOOOOOOOOOOO")
+                    print("ACCESS GRANTED")
+                    subprocess.call(["xdg-open", '/home/pi'])
                     break
                 else:
-                    continue
+                    print("ACCESS DENIED")
+
             else:
                 print("wrong password try again")
+
+
                 
     elif(GPIO.input(button) == GPIO.LOW):
         print("Push the button to record audio...")
-        sleep(5)
+        
+        while(GPIO.input(button) == GPIO.LOW):
+            sleep(0.1)
         if(GPIO.input(button) == GPIO.HIGH):
-            running = True
-        else:
-            running = False
+            continue
+
+
 
 window.mainloop()
+
+        
+
